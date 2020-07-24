0096e410f2fd5c98a6ccd64c6ba4b7db60099edb
shellus <shellus@endaosi.com>
2020-07-24 11:48:39 +0800

    完成docker

---
+++ docker-compose/docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -eo pipefail
+shopt -s nullglob
+
+# 检查权限
+#chown -R mysql:mysql /var/lib/mysql
+
+if [ "`ls /var/lib/mysql`" = "" ]; then
+    echo 'init mysql data dir...'
+    # 初始化数据库 initialize-insecure 才是初始化并空root密码
+    mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql
+
+    echo 'init mysql data dir success !'
+
+    # 运行并创建数据库
+    echo 'starting mysqld ...'
+    /bin/bash -c "mysqld &" && sleep 5
+    echo 'mysqld started !'
+
+    echo 'create database and create mysql user ...'
+    mysql -e "create database proxy_manager; CREATE USER 'root'@'127.0.0.1'; GRANT ALL ON *.* TO  'root'@'127.0.0.1'; FLUSH PRIVILEGES;"
+    echo 'database created !'
+
+    # 迁移数据
+    echo 'running migrate ...'
+    cd /webroot/proxy-manager/ && chmod -R 777 storage && php artisan migrate
+    echo 'migrated ...'
+
+    # 停止mysql
+    killall mysqld
+else
+    echo 'found exists mysql data dir !'
+fi
+
+echo 'running supervisord ...'
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/', function () {
+    return "<h1>API entry point </h1>";
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
